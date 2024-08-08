931a323c33add454860cefcc5555b6c491ce276f
Kai Zheng <kaizheng9000@outlook.com>
2024-08-08 17:56:42 -0600

    ignoring vscode folder

---
+++ Merge Strings Alternately/MergeStringsAlternately.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+
+// int solve(std::string word1, std::string word2)
+// {
+
+//     return 0;
+// }
+
+
+int main()
+{
+    std::string word1 = "abc";
+    std::cout << "hello";
+    return 0;
+}
