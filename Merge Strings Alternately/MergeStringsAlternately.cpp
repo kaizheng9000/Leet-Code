a301b4680222d4dd840032c1c4790a6896f2e015
Kai Zheng <kaizheng9000@outlook.com>
2024-08-13 18:46:39 -0600

    Completed LeetCode 1768 - Merge Strings Alternately

--- Merge Strings Alternately/MergeStringsAlternately.cpp
+++ Merge Strings Alternately/MergeStringsAlternately.cpp
@@ -1,18 +1,37 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 
-// int solve(std::string word1, std::string word2)
-// {
+std::string solve(std::string word1, std::string word2)
+{
+    std::ostringstream result;
+    
+    int min = std::min(word1.length(), word2.length());
 
-//     return 0;
-// }
+    for(int i = 0; i < min; i++)
+    {
+        result << word1[i] << word2[i];
+    }
+
+    if(min == word1.length())
+    {
+        result << word2.substr(min);
+    }
+    else
+    {
+        result << word1.substr(min);
+    }
+
+    return result.str();
+}
 
 
 int main()
 {
-    std::string word1 = "abc";
-    std::cout << "hello";
+    std::string word = "abc";
+    std::string word2 = "pqrstuv";
+    std::cout << solve(word, word2);
     return 0;
 }
