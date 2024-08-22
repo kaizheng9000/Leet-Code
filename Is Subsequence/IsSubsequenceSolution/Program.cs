72005755b452a4ddb156fa963d9363703051fbbd
Kai Zheng <kaizheng9000@outlook.com>
2024-08-22 10:26:04 -0600

    Finished Is Subsequence

---
+++ Is Subsequence/IsSubsequenceSolution/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace IsSubsequenceSolution;
+
+public class IsSubsequenceSolution
+{
+    public static void Main(string[] args)
+    {
+        string sequence = "abc";
+        string sub = "b";
+
+        Console.WriteLine(Solve(sub, sequence));
+    }
+
+    private static bool Solve(string s, string t)
+    {
+        if(String.IsNullOrEmpty(s))
+        {
+            return true;
+        }
+
+        int matchCounter = 0;
+        foreach(char letter in t)
+        {
+            if(matchCounter != s.Length && s[matchCounter].Equals(letter))
+            {
+                matchCounter++;
+            }
+        }
+        return matchCounter == s.Length;
+    }
+    
+
+}
