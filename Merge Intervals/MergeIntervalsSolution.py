4d2d32db6de00f9b6dc250cbc077b106a25cf590
Kai Zheng <kaizheng9000@outlook.com>
2024-09-24 17:08:48 -0600

    Completed Merge Intervals

--- Merge Intervals/MergeIntervalsSolution.py
+++ Merge Intervals/MergeIntervalsSolution.py
@@ -1,6 +1,7 @@
 
 example = [[1,3],[2,6],[8,10],[15,18]]
 example2 = [[1,4],[4,5]]
+example3 = [[1,4],[2,3]]
 
 def merge_intervals(intervals):
 
@@ -9,21 +10,14 @@ def merge_intervals(intervals):
 
     result = []
     sorted_list = sorted(intervals)
-    lower = sorted_list[0][0]
-    upper = sorted_list[0][1]
-    current = []
 
-    for i in range(1, len(sorted_list)):
-        if sorted_list[i][0] <= upper:
-            # max_upper = ma
-            current = [lower, sorted_list[i][1]]
-            result.append(current)
-            lower = sorted_list[i][0]
-            upper = sorted_list[i][1]
-        else:
-            if i == 1:
-                result.append(sorted_list[0])
-            result.append(sorted_list[i])
+    for interval in sorted_list:
+        if not result or result[-1][1] < interval[0]: # Check for overlap
+            result.append(interval)
+        else:   # Need to merge
+            result[-1] = [result[-1][0], max(result[-1][1], interval[1])]
+
     return result
+
 
 print(merge_intervals(example))
