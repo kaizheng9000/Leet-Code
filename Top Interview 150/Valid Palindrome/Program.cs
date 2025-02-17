3758de9f525a19a307ae440e428c4b991f207ac1
Kai Zheng <kaizheng9000@outlook.com>
2025-02-16 19:46:36 -0700

    Finished Valid Palindrome

---
+++ Top Interview 150/Valid Palindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public static int Main(string[] args)
+    {
+        string sequence = "1a2";
+
+        Solution solve = new Solution();
+
+        Console.WriteLine(solve.isPalindrome(sequence));
+
+        return 0;
+    }
+
+    public bool isPalindrome(string s)
+    {
+        string cleaned = Regex.Replace(s, @"[^A-Za-z0-9]+", "");
+        string lowered = cleaned.ToLower();
+
+        int left = 0;
+        int right  = lowered.Length - 1;
+
+        while (left < right)
+        {
+            if (lowered[left].CompareTo(lowered[right]) != 0)
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+
+
