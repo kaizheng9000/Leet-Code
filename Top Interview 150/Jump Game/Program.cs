2eee537d543fc7ccefb3a803a602e76431956eb4
Kai Zheng <kaizheng9000@outlook.com>
2025-02-17 19:55:34 -0700

    Finished Jump Game

---
+++ Top Interview 150/Jump Game/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] example = [2,3,1,1,4];
+
+        Solution solve = new Solution();
+        Console.WriteLine(solve.CanJump(example));
+    }
+
+    public bool CanJump(int[] nums)
+    {
+        int jumpDistance = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (jumpDistance < 0)
+            {
+                return false;
+            }
+            else if (nums[i] > jumpDistance)
+            {
+                jumpDistance = nums[i];
+            }
+
+            jumpDistance--;
+        }
+
+        return true;
+    }
+}
