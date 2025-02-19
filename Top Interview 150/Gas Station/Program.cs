322874eee99358056c0ffe20bc80d7a7bd048241
Kai Zheng <kaizheng9000@outlook.com>
2025-02-18 23:14:17 -0700

    Finished Gas Station

---
+++ Top Interview 150/Gas Station/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] gas = [1,2,3,4,5];
+        int[] cost = [3,4,5,1,2];
+;
+
+        Solution solve = new Solution();
+        Console.WriteLine(solve.CanCompleteCircuit(gas, cost));
+    }
+
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        if (gas.Sum() < cost.Sum())
+        {
+            return -1;
+        }
+
+        int currGas = 0;
+        int startPos = 0;
+
+        for(int i = 0; i < gas.Length; i++)
+        {
+            currGas += gas[i] - cost[i];
+            if (currGas < 0)
+            {
+                currGas = 0;
+                startPos = i + 1;
+            }
+        }
+
+        return startPos;
+    }
+}
